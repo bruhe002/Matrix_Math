04a99ba1ea9fa771273df24ef1bc07d86a60edb4
Eric Bruhns <bruhe002@gmail.com>
2023-03-19 16:51:26 -0700

    Made a constant integer size

--- stack_version.cpp
+++ stack_version.cpp
@@ -5,20 +5,23 @@
 
 using namespace std;
 
-void fillMatrix(int matrix[100][100]);
-void printMatrix(int matrix[100][100]);
+const int ARRAY_SIZE = 10;
 
-void addMatrix(int a[100][100], int b[100][100], int c[100][100]);
-void subMatrix(int a[100][100], int b[100][100], int c[100][100]);
-void multMatrix(int a[100][100], int b[100][100], int c[100][100]);
+void fillMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]);
+void printMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]);
+
+void addMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]);
+void subMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]);
+void multMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]);
 
 int main() {
-
-    int matrix1[100][100];
-    int matrix2[100][100];
-    int sumMatrix[100][100];
-    int diffMatrix[100][100];
-    int productMatrix[100][100];
+    srand(time(NULL));
+    
+    int matrix1[ARRAY_SIZE][ARRAY_SIZE];
+    int matrix2[ARRAY_SIZE][ARRAY_SIZE];
+    int sumMatrix[ARRAY_SIZE][ARRAY_SIZE];
+    int diffMatrix[ARRAY_SIZE][ARRAY_SIZE];
+    int productMatrix[ARRAY_SIZE][ARRAY_SIZE];
 
     fillMatrix(matrix1);
     fillMatrix(matrix2);
@@ -36,18 +39,17 @@ int main() {
     return 0;
 }
 
-void fillMatrix(int matrix[100][100]){
-    srand(time(NULL));
-    for(int i = 0; i < 100; i++) {
-        for(int j = 0; j < 100; j++) {
+void fillMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]){
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+        for(int j = 0; j < ARRAY_SIZE; j++) {
             matrix[i][j] = rand() % 9 + 1;
         }
     }
 }
 
-void printMatrix(int matrix[100][100]) {
-    for(int i = 0; i < 100; i++) {
-        for(int j = 0; j < 100; j++) {
+void printMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]) {
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+        for(int j = 0; j < ARRAY_SIZE; j++) {
             cout << matrix[i][j] << " ";
         }
 
