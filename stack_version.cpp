7d1c445171fdce24108e8486c95ad04e5b375b4a
Eric Bruhns <bruhe002@gmail.com>
2023-03-28 13:42:05 -0700

    Made the changes to stack_version.cpp

--- stack_version.cpp
+++ stack_version.cpp
@@ -1,71 +1,151 @@
 #include <iostream>
+#include <sstream>
+#include <exception>
 #include <array>
+#include <string>
 #include <stdlib.h>
-#include <ctime>
 
 using namespace std;
 
-const int ARRAY_SIZE = 10;
+const int MAX_ARRAY_SIZE = 100;
 
-void fillMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]);
-void printMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]);
+bool exitCreateMatrixFlag = true;
+bool exitMenuFlag = false;
 
-void addMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]);
-void subMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]);
-void multMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]);
+void fillMatrix(float matrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size);
+void printMatrix(float matrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size);
+void menuFunction(int choice, float matrix1[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float matrix2[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+                    float resultMatrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size);
+
+void addMatrix(float a[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float b[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float c[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size);
+void subMatrix(float a[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float b[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float c[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size);
+void multMatrix(float a[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float b[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float c[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size);
 
 int main() {
-    srand(time(NULL));
+    cout << "\nWelcome to the MATRIX CALCULATOR" << endl;
+    // Initialize Matrices
+    float matrix1[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE];
+    float matrix2[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE];
+    float resultingMatrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE];
 
-    int matrix1[ARRAY_SIZE][ARRAY_SIZE];
-    int matrix2[ARRAY_SIZE][ARRAY_SIZE];
-    int sumMatrixResult[ARRAY_SIZE][ARRAY_SIZE];
-    int diffMatrixResult[ARRAY_SIZE][ARRAY_SIZE];
-    int productMatrixResult[ARRAY_SIZE][ARRAY_SIZE];
+    do {
+        // reset flag
+        exitCreateMatrixFlag = true;
+        exitMenuFlag = false;
 
-    fillMatrix(matrix1);
-    fillMatrix(matrix2);
+        cout << "\nPlease enter the size of the matrix: ";
+        string garbage;
+        int matrixSize = 0;
+        cin >> matrixSize;
+        getline(cin, garbage);
 
-    cout << "Matrix 1" << endl;
-    printMatrix(matrix1);
+        if(matrixSize < MAX_ARRAY_SIZE) {
+            // Ask user for inputs row by row
+            cout << "\nNow let's enter some values into your FIRST MATRIX:\n" << endl;
+            fillMatrix(matrix1, matrixSize);
 
+            cout << "\nNow let's enter some values into your SECOND MATRIX:\n" << endl;
+            fillMatrix(matrix2, matrixSize);
+
+            // Display Menu
+            do {
+                cout << "Please choose the following: " << endl;
+                cout << "\t1. Add your Matrices" << endl;
+                cout << "\t2. Subtract your Matrices" << endl;
+                cout << "\t3. Multiply your matrices" << endl;
+                cout << "\t4. Change your Matrices" << endl;
+                cout << "\t5. Exit" << endl;
+
+                try {
+                    int userChoice = 0;
+                    cin >> userChoice;
+                    if(userChoice != 1 && userChoice != 2 && userChoice != 3 && userChoice != 4 && userChoice != 5) {
+                        cout << "Not a valid choice! Please try again!" << endl;
+                    } else {
+                        // cout << "Entering menu Function" << endl;
+                        menuFunction(userChoice, matrix1, matrix2, resultingMatrix, matrixSize);
+                        if(resultingMatrix != nullptr) {
+                            cout << "Result:" << endl;
+                            printMatrix(resultingMatrix, matrixSize);
+                        }
+                    }
+                } catch(const exception& e) {
+                    cerr << e.what() << endl;
+                }
+
+            } while (!exitMenuFlag);
+        } else {
+            cerr << "The Size needs to be less than 100!" << endl << "Please Try Again!" << endl;
+            exitCreateMatrixFlag = false;
+        }
+
+        cout << endl;
+        
+    } while (!exitCreateMatrixFlag);
+
+    
     cout << endl;
-
-    cout << "Matrix 2" << endl;
-    printMatrix(matrix2);
-    cout << endl;
-
-    cout << "Sum Matrix" << endl;
-    addMatrix(matrix1, matrix2, sumMatrixResult);
-    printMatrix(sumMatrixResult);
-    cout << endl;
-
-    cout << "Difference Matrix" << endl;
-    subMatrix(matrix1, matrix2, diffMatrixResult);
-    printMatrix(diffMatrixResult);
-    cout << endl;
-
-    cout << "Product Matrix" << endl;
-    multMatrix(matrix1, matrix2, productMatrixResult);
-    printMatrix(productMatrixResult);
-    cout << endl;
+    system("Pause");
+    return 0;
 
     cout << endl;
     system("Pause");
     return 0;
 }
 
-void fillMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]){
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        for(int j = 0; j < ARRAY_SIZE; j++) {
-            matrix[i][j] = rand() % 9 + 1;
+void menuFunction(int choice, float matrix1[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float matrix2[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+                    float resultMatrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size) {
+    switch(choice) {
+        case 1: 
+            addMatrix(matrix1, matrix2, resultMatrix, size);
+            break;
+        case 2:
+            subMatrix(matrix1, matrix2, resultMatrix, size);
+            break;
+        case 3:
+            multMatrix(matrix1, matrix2, resultMatrix, size);
+            break;
+        case 4:
+            exitCreateMatrixFlag = false;
+            exitMenuFlag = true;
+            break;
+        default:
+            exitMenuFlag = true;
+            exitCreateMatrixFlag = true;
+            break;
+    }
+}
+
+void fillMatrix(float matrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size){
+    string row;
+    for(int i = 0; i < size; i++) {
+        cout << "For ROW " << i << ": ";
+        getline(cin, row);
+        cout << endl;
+
+        int elementCounter = 0;
+        string element;
+        stringstream input(row);
+        while(getline(input, element, ' ')) {
+            // If elementCounter Does not exceed the size of the row
+            if(elementCounter < size) {
+                matrix[i][elementCounter] = stof(element);
+                elementCounter++;
+            }
+
+        }
+
+        // If the number of elements did not reach the row size
+        // Padd with zeroes
+        for(int j = elementCounter; j < size; j++) {
+            matrix[i][j] = 0.0;
         }
     }
 }
 
-void printMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]) {
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        for(int j = 0; j < ARRAY_SIZE; j++) {
+void printMatrix(float matrix[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size) {
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
             cout << matrix[i][j] << " ";
         }
 
@@ -73,28 +153,28 @@ void printMatrix(int matrix[ARRAY_SIZE][ARRAY_SIZE]) {
     }
 }
 
-void addMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]) {
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        for(int j = 0; j < ARRAY_SIZE; j++) {
+void addMatrix(float a[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float b[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float c[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size) {
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
             c[i][j] = a[i][j] + b[i][j];
         }
     }
 }
 
-void subMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]) {
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        for(int j = 0; j < ARRAY_SIZE; j++) {
+void subMatrix(float a[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float b[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float c[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size) {
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
             c[i][j] = a[i][j] - b[i][j];
         }
     }
 }
 
-void multMatrix(int a[ARRAY_SIZE][ARRAY_SIZE], int b[ARRAY_SIZE][ARRAY_SIZE], int c[ARRAY_SIZE][ARRAY_SIZE]) {
+void multMatrix(float a[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float b[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], float c[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int size) {
     
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        for(int j = 0; j < ARRAY_SIZE; j++) {
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
             c[i][j] = 0;
-            for(int k = 0; k < ARRAY_SIZE; k++) {
+            for(int k = 0; k < size; k++) {
                 int temp = a[i][k] * b[k][j];
                 c[i][j] += temp; 
             }
